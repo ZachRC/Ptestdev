6ca0ecd2c033529e0436dc5e2c2218f9c0a89607
ZacRC <1234rack1234@gmail.com>
2025-01-16 02:04:31 -0600

    Updated database configuration to use direct parameters

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,41 +9,46 @@ data_path="./certbot"
 email="zacharyrcherney@gmail.com"
 staging=1 # Set to 0 once testing is complete
 
-echo "### Creating directory structure"
-mkdir -p "$data_path/conf/live/$domains"
+# Make sure docker-compose is down
+docker-compose down
 
+# Remove any existing certificates
+rm -rf "$data_path"
+
+# Create the required directories
+mkdir -p "$data_path/www"
+mkdir -p "$data_path/conf/live/solforge.live"
+
+# Download TLS parameters
 echo "### Downloading recommended TLS parameters ..."
 curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
 curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
 
-echo "### Removing old certificates ..."
-docker-compose down
-rm -rf "$data_path/conf/live"
-mkdir -p "$data_path/conf/live"
+# Create dummy certificates
+echo "### Creating dummy certificate for ${domains[0]} ..."
+openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+  -keyout "$data_path/conf/live/solforge.live/privkey.pem" \
+  -out "$data_path/conf/live/solforge.live/fullchain.pem" \
+  -subj '/CN=localhost' 2>/dev/null
 
-echo "### Creating dummy certificate ..."
-openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 \
-    -keyout "$data_path/conf/live/$domains/privkey.pem" \
-    -out "$data_path/conf/live/$domains/fullchain.pem" \
-    -subj '/CN=localhost'
-
+# Start nginx
 echo "### Starting nginx ..."
 docker-compose up -d nginx
 
+# Wait for nginx to start
 echo "### Waiting for nginx to start ..."
-sleep 5
+sleep 10
 
+# Request the real certificate
 echo "### Requesting Let's Encrypt certificate for ${domains[*]} ..."
-docker-compose run --rm certbot certonly \
-    --webroot \
-    --webroot-path=/var/www/certbot \
+docker-compose run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
     --email $email \
     --agree-tos \
     --no-eff-email \
     --staging \
     --force-renewal \
-    ${staging:+"--staging"} \
-    ${domains[@]/#/-d }
+    -d solforge.live -d www.solforge.live" certbot
 
 echo "### Reloading nginx ..."
 docker-compose exec nginx nginx -s reload 
